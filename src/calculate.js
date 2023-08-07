e7cf73e18ab69e243dc01ba21bb1473613802655
Wint-Nandi-Soe <135989754+Wint-Nandi-Soe@users.noreply.github.com>
2023-08-07 22:13:50 +0630

    Updated Form Function

--- src/calculate.js
+++ src/calculate.js
@@ -13,25 +13,35 @@ function displayDate(time){
     return `${day} ${hour}:${minutes}`;
 }
 function displayTemperature (response){
-    console.log(response.data);
     let tempElement=document.querySelector("#temperature");
-    tempElement.innerHTML=Math.round(response.data.main.temp);
     let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=response.data.name;
     let descElement= document.querySelector("#description");
-    descElement.innerHTML=response.data.weather[0].description;
     let humiElement = document.querySelector("#humidity");
-    humiElement.innerHTML= response.data.main.humidity;
     let windElement = document.querySelector("#wind");
-    windElement.innerHTML=response.data.wind.speed;
     let dateElement=document.querySelector("#date");
-    dateElement.innerHTML=displayDate(response.data.dt*1000);
     let iconElement=document.querySelector("#icon");
+    cityElement.innerHTML=response.data.name;
+    tempElement.innerHTML=Math.round(response.data.main.temp);
+    descElement.innerHTML=response.data.weather[0].description;
+    humiElement.innerHTML= response.data.main.humidity;
+    windElement.innerHTML=response.data.wind.speed;
+    dateElement.innerHTML=displayDate(response.data.dt*1000);
     iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description);
-    
-
 }
-let apiKey = "f25f47a5f94b046a3902e21ded66f15f";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Yangon&appid=${apiKey}&units=metric`;
-axios.get(apiURL).then(displayTemperature);
+function searchCity(city){
+    let apiKey = "f25f47a5f94b046a3902e21ded66f15f";
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiURL).then(displayTemperature);
+}
+
+function displayName(event){
+    event.preventDefault();
+    let cityName=document.querySelector("#city-input");
+    searchCity(cityName.value);
+}
+
+
+//Form Section
+let form = document.querySelector("#class-form");
+form.addEventListener("submit", displayName);
